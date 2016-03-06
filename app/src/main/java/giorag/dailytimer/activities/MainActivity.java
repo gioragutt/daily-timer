63e062b0d9a8abb92d8574482ff977120c2b0abe
Giora Guttsait <giora111@gmail.com>
2016-03-06 02:28:42 +0200

    Added daily logic, changed MainActivity to run with daily logic, needs review from ron green! also needs refactoring

--- app/src/main/java/giorag/dailytimer/activities/MainActivity.java
+++ app/src/main/java/giorag/dailytimer/activities/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -16,75 +18,77 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import java.util.ArrayList;
 
-import giorag.dailytimer.DailyCountdown;
+import giorag.dailytimer.Daily;
 import giorag.dailytimer.R;
-import giorag.dailytimer.enums.RunningState;
+import giorag.dailytimer.enums.BufferType;
 import giorag.dailytimer.interfaces.TeamNamesEditDialogListener;
 import giorag.dailytimer.TinyDB;
 import giorag.dailytimer.modals.Person;
 import giorag.dailytimer.modals.Time;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, TeamNamesEditDialogListener {
+        implements NavigationView.OnNavigationItemSelectedListener, TeamNamesEditDialogListener, Daily.OnDailyFinishListener
+{
 
     public static final String CMD_PLAY = "{cmd-play}";
     public static final String CMD_REPLAY = "{cmd-replay}";
     public static final String CMD_PAUSE = "{cmd-pause}";
     public static final String CMD_STOP = "{cmd-stop}";
-    Time defaultTime;
+    long totalTime;
+    long speakingTime;
+    long bufferTime;
+    BufferType bufferType;
+
     Button pause;
     Button start;
-    TextView timer;
+    ImageButton next;
+
+    TextView totalTimer;
+    TextView personalTimer;
+    TextView bufferTimer;
+    TextView participantLabel;
     TinyDB db;
+    Daily daily;
     SharedPreferences preferences;
-    DailyCountdown countdown;
     ArrayList<Person> people;
-    RunningState runningState;
+
 
     @Override
     public void onPersonsListUpdate(ArrayList<Person> people) {
         this.people = people;
-        int peopleAmount = 0;
-        for (Person p : people) {
+        ArrayList<Person> availablePeople = getAvailablePeople();
+
+        initializeTimerTime(availablePeople.size());
+        initializeDaily(availablePeople);
+        initializeViews();
+    }
+
+    @NonNull
+    private ArrayList<Person> getAvailablePeople() {
+        ArrayList<Person> availablePeople = new ArrayList<>();
+        for (Person p : this.people) {
             if (p.available) {
-                peopleAmount++;
+                availablePeople.add(p);
             }
         }
-
-        initializeTimerTime(peopleAmount);
-        initializeViews();
+        return availablePeople;
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         log("onSaveInstanceState");
-        outState.putLong("remaining", countdown.getRemaining());
-        outState.putLong("interval", countdown.getInterval());
-        outState.putString("runningState", runningState.toString());
-        log("onSaveInstanceState - remaining " + countdown.getRemaining());
-        log("onSaveInstanceState - interval " + countdown.getInterval());
-        log("onSaveInstanceState - runningState " + runningState);
+        daily.saveState(outState);
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         log("onRestoreInstanceState");
-        long remaining = savedInstanceState.getLong("remaining");
-        long interval = savedInstanceState.getLong("interval");
-        RunningState runningState = RunningState.valueOf(savedInstanceState.getString("runningState"));
-
-        log("onRestoreInstanceState - remaining " + remaining);
-        log("onRestoreInstanceState - interval " + interval);
-        log("onRestoreInstanceState - runningState " + runningState);
-
-        this.runningState = runningState;
-        resetCountdown(runningState, remaining, interval);
-        runningState.restore(this);
-        setTimerText(Time.fromLong(remaining));
+        daily.restoreState(this, savedInstanceState);
         super.onRestoreInstanceState(savedInstanceState);
     }
 
@@ -107,23 +111,39 @@ public class MainActivity extends AppCompatActivity
         db = new TinyDB(this);
         initializeSettings();
 
+        log("Speaking time : " + Time.fromLong(speakingTime).toString());
+        log("Total time : " + Time.fromLong(totalTime).toString());
+        log("Buffer time : " + Time.fromLong(bufferTime).toString());
+
         pause = (Button)findViewById(R.id.main_pause);
         start = (Button)findViewById(R.id.main_start);
-        timer = (TextView)findViewById(R.id.main_timer);
+        next = (ImageButton)findViewById(R.id.move_to_next_person);
+        totalTimer = (TextView)findViewById(R.id.main_timer);
+        personalTimer = (TextView)findViewById(R.id.person_timer);
+        bufferTimer = (TextView)findViewById(R.id.buffer_timer);
+        participantLabel = (TextView)findViewById(R.id.current_practicipent_name);
 
-        runningState = RunningState.Default;
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNextClick();
+            }
+        });
 
+        initializeDaily(getAvailablePeople());
         initializeViews();
+    }
+
+    private void initializeDaily(ArrayList<Person> availablePeople) {
+        daily = new Daily(Time.fromLong(totalTime), Time.fromLong(speakingTime), Time.fromLong(bufferTime),
+                availablePeople, bufferType, 0, totalTimer, personalTimer, bufferTimer, participantLabel);
+
+        daily.setOnDailyFinishListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        if(runningState == RunningState.Default) {
-            initializeSettings();
-            initializeViews();
-        }
     }
 
     @Override
@@ -159,6 +179,44 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+
+    private void setNextButton(boolean enabled) {
+        next.setEnabled(enabled);
+        next.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_action_playback_forw));
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNextClick();
+            }
+        });
+    }
+
+    private void onNextClick() {
+        if (daily.isNotLastPerson())
+            daily.next();
+        else {
+            daily.finish();
+            setRestartButton();
+        }
+    }
+
+    private void setRestartButton() {
+        next.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_autorenew));
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRestartClick();
+            }
+        });
+    }
+
+    private void onRestartClick() {
+        onResetClick();
+        next.setEnabled(false);
+        start.setEnabled(true);
+    }
+
     private void setStartButton(boolean enabled) {
         start.setText(CMD_PLAY);
         start.setEnabled(enabled);
@@ -172,10 +230,10 @@ public class MainActivity extends AppCompatActivity
 
     public void onStartClick() {
         log("Pressed START!");
-        countdown.start();
-        runningState = RunningState.Running;
+        daily.start();
         start.setEnabled(false);
         setPauseButton(true);
+        setNextButton(true);
     }
 
     private void setResumeButton(boolean enabled) {
@@ -191,8 +249,7 @@ public class MainActivity extends AppCompatActivity
 
     public void onResumeClick() {
         log("Pressed RESUME!");
-        runningState = RunningState.Running;
-        resetCountdown(true);
+        daily.start();
         start.setEnabled(false);
         setPauseButton(true);
     }
@@ -210,8 +267,7 @@ public class MainActivity extends AppCompatActivity
 
     public void onPauseClick() {
         log("Pressed PAUSE!");
-        runningState = RunningState.Paused;
-        resetCountdown(false);
+        daily.pause();
         setResumeButton(true);
         setResetButton(true);
     }
@@ -229,8 +285,10 @@ public class MainActivity extends AppCompatActivity
 
     public void onResetClick() {
         log("Pressed RESET!");
-        initializeViews();
-        runningState = RunningState.Default;
+        daily.reset();
+        setStartButton(true);
+        setPauseButton(false);
+        next.setEnabled(false);
     }
 
     private void initializeSettings() {
@@ -251,66 +309,45 @@ public class MainActivity extends AppCompatActivity
             peopleAmount = 0;
         }
 
-        if (peopleAmount < 2)
-            peopleAmount = 2;
-
         initializeTimerTime(peopleAmount);
     }
 
     private void initializeTimerTime(int peopleAmount) {
-        int speakingTime = Integer.parseInt(preferences.getString("speaking_time", "30"));
+        speakingTime = Long.parseLong(preferences.getString("speaking_time", "30")) * 1000;
         int transitionBuffer = Integer.parseInt(preferences.getString("transition_buffer", "0"));
-        int transitionBufferTime = Integer.parseInt(preferences.getString("transition_buffer_time", "5"));
+        bufferTime = Long.parseLong(preferences.getString("transition_buffer_time", "5")) * 1000;
 
         long totalTime = peopleAmount * speakingTime;
         switch (transitionBuffer) {
             case 0: {
-                totalTime += transitionBufferTime;
+                bufferType = BufferType.Team;
+                totalTime += bufferTime;
                 break;
             }
             case 1: {
-                totalTime += transitionBufferTime * peopleAmount;
+                bufferType = BufferType.Individual;
+                totalTime += bufferTime * peopleAmount;
                 break;
             }
             default: break;
         }
 
-        totalTime *= 1000;
-        defaultTime = Time.fromLong(totalTime);
+        this.totalTime = totalTime;
+
+        log("Speaking time : " + Time.fromLong(speakingTime).toString());
+        log("Total time : " + Time.fromLong(this.totalTime).toString());
+        log("Buffer time : " + Time.fromLong(bufferTime).toString());
     }
 
     private void log(String message) {
         Log.i("Daily-Timer", message);
     }
 
-    public void setTimerText(Time time) {
-        timer.setText(time.toString());
-    }
-
-    public TextView getTimer() {
-        return timer;
-    }
-
-    private void resetCountdown(boolean startOnReset) {
-        RunningState state = startOnReset ? RunningState.Running : RunningState.Default;
-        resetCountdown(state, countdown.getRemaining(), countdown.getInterval());
-    }
-
-    private void resetCountdown(RunningState runningState, long remaining, long interval) {
-        countdown.cancel();
-        countdown = new DailyCountdown(Time.fromLong(remaining), interval, timer);
-        if (runningState == RunningState.Running)
-            countdown.start();
-    }
-
     public void initializeViews() {
-        if (countdown != null)
-            countdown.cancel();
-        countdown = new DailyCountdown(defaultTime, 100, timer);
-        setTimerText(defaultTime);
-        runningState = RunningState.Default;
+        daily.reset();
         setStartButton(true);
         setPauseButton(false);
+        next.setEnabled(false);
     }
 
     public void launchSettings(MenuItem item) {
@@ -347,6 +384,13 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFinish() {
+        setRestartButton();
+        setStartButton(false);
+        setPauseButton(false);
     }
 }
 
