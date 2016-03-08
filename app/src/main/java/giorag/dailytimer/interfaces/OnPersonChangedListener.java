c53fb8d0a00ed89edd9095d4d759eeef6887e50d
Giora Guttsait <giora111@gmail.com>
2016-03-08 21:12:32 +0200

    Moved OnDailyFinishListener and OnPersonChangedListener from Daily to own files

---
+++ app/src/main/java/giorag/dailytimer/interfaces/OnPersonChangedListener.java
@@ -0,0 +1,10 @@
+package giorag.dailytimer.interfaces;
+
+import giorag.dailytimer.modals.Person;
+
+/**
+ * Created by GioraPC on 08/03/2016.
+ */
+public interface OnPersonChangedListener {
+    void onPersonChanged(Person newPerson);
+}
